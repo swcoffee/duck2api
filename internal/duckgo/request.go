5818b6ea3f8b2c75e1be4b83ee2528c743c294d7
swcoffee <swcoffee@qq.com>
2024-09-03 15:22:04 +0800

    fix:modify ua

--- internal/duckgo/request.go
+++ internal/duckgo/request.go
@@ -19,7 +19,7 @@ import (
 
 var (
 	Token   *XqdgToken
-	UA      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+	UA      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
 	Gateway string
 )
 
@@ -129,7 +129,7 @@ func createHeader() httpclient.AuroraHeaders {
 	header.Set("content-type", "application/json")
 	header.Set("origin", Gateway)
 	header.Set("referer", Gateway)
-	header.Set("sec-ch-ua", `"Chromium";v="120", "Google Chrome";v="120", "Not-A.Brand";v="99"`)
+	header.Set("sec-ch-ua", `"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"`)
 	header.Set("sec-ch-ua-mobile", "?0")
 	header.Set("sec-ch-ua-platform", `"Windows"`)
 	header.Set("sec-fetch-dest", "empty")
